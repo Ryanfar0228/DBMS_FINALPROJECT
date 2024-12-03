508d744e8e4f697686c818aeabdf496bfe088b38
Ryanfar0228 <148507483+Ryanfar0228@users.noreply.github.com>
2024-12-03 17:23:07 -0500

    Create custom set/ add&delete parts / search parts&sets
    functionalities just need to be added to the html

--- LegoJS/app.js
+++ LegoJS/app.js
@@ -65,6 +65,86 @@ app.post('/search', (req, res) => {
 });
 
 //*******************************************************************************
+//*** Create custom sets POST
+//*******************************************************************************
+app.post('/create-set', (req, res) => {
+  const { set_name } = req.body; // Get the custom set name from the form
+  const query = 'INSERT INTO custom_sets (name) VALUES (?)';
+
+  db.query(query, [set_name], (err, result) => {
+    if (err) {
+      console.error('Error creating custom set:', err.message);
+      return res.status(500).send('<p>Error creating custom set</p>');
+    }
+
+    res.send(`<p>Custom set "${set_name}" created successfully!</p><br><a href="/">Back to Home</a>`);
+  });
+});
+
+
+//*******************************************************************************
+//*** Add parts to set POST
+//*******************************************************************************
+app.post('/add-part', (req, res) => {
+  const { set_id, part_num, quantity } = req.body; // Extract data from the form
+  const query = 'INSERT INTO custom_set_parts (set_id, part_num, quantity) VALUES (?, ?, ?)';
+
+  db.query(query, [set_id, part_num, quantity], (err, result) => {
+    if (err) {
+      console.error('Error adding part to custom set:', err.message);
+      return res.status(500).send('<p>Error adding part to custom set</p>');
+    }
+
+    res.send(`<p>Part "${part_num}" added to set ID "${set_id}"!</p><br><a href="/">Back to Home</a>`);
+  });
+});
+
+
+//*******************************************************************************
+//*** Delete parts from set POST
+//*******************************************************************************
+app.post('/delete-part', (req, res) => {
+  const { set_id, part_num } = req.body; // Extract data from the form
+  const query = 'DELETE FROM custom_set_parts WHERE set_id = ? AND part_num = ?';
+
+  db.query(query, [set_id, part_num], (err, result) => {
+    if (err) {
+      console.error('Error deleting part from custom set:', err.message);
+      return res.status(500).send('<p>Error deleting part from custom set</p>');
+    }
+
+    res.send(`<p>Part "${part_num}" removed from set ID "${set_id}"!</p><br><a href="/">Back to Home</a>`);
+  });
+});
+
+
+//*******************************************************************************
+//*** Search set POST
+//*******************************************************************************
+app.post('/search-set', (req, res) => {
+  const { search } = req.body; // Get search term
+  const query = 'SELECT set_num, name, year FROM sets WHERE name LIKE ? OR set_num LIKE ? LIMIT 10';
+
+  db.query(query, [`%${search}%`, `%${search}%`], (err, results) => {
+    if (err) {
+      console.error('Error searching sets:', err.message);
+      return res.status(500).send('<p>Error searching sets</p>');
+    }
+
+    // Generate HTML response
+    let html = `<h1>Search Results for "${search}"</h1><table border="1">`;
+    html += '<tr><th>Set Number</th><th>Name</th><th>Year</th></tr>';
+    results.forEach((set) => {
+      html += `<tr><td>${set.set_num}</td><td>${set.name}</td><td>${set.year}</td></tr>`;
+    });
+    html += '</table><br><a href="/">Back to Home</a>';
+
+    res.send(html);
+  });
+});
+
+
+//*******************************************************************************
 //*** Start the Server
 //*******************************************************************************
 app.listen(PORT, () => {
