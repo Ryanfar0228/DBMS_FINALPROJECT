f19fbcf5633d812c016ad5d277c9a9f01e77c353
philkershner <pk745160@sju.edu>
2024-12-03 13:15:13 -0500

    Update backend.js

--- LegoJS/backend.js
+++ LegoJS/backend.js
@@ -183,3 +183,67 @@ router.get('/:theme_id', (req, res) => {
 });
 
 module.exports = router;
+
+/*
+Forouraghi method
+*/
+
+const fs = require("fs");
+
+function readAndServe(path, res)
+{
+    fs.readFile(path,function(err, data) {
+
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    })
+}
+
+//******************************************************************************
+//*** receive the main page get requests from the client
+//******************************************************************************
+app.get("/search", function (req, res) {
+    readAndServe("./search.html",res)   //uses directory structure
+
+});
+
+
+//******************************************************************************
+//*** receive post register data from the client
+//******************************************************************************
+app.post("/search", function (req, res) {
+    var desc = req.body.desc;   // extract the strings received from the browser
+
+    var sql_query = "select title, description from film where description like '%" + desc + "%'";
+
+    con.query(sql_query, function (err, result, fields) { // execute the SQL string
+		if (err)
+		    throw err;                  // SQL error
+
+	    else {
+
+                  //*** start creating the html body for the browser
+			      var html_body = "<HTML><STYLE>body{font-family:arial}</STYLE>";
+			      html_body = html_body + "<BODY><TABLE BORDER=1>";
+
+			      //*** print column headings
+			      html_body = html_body + "<TR>";
+                 for (var i = 0; i < fields.length; i++)
+				    html_body = html_body + ("<TH>" + fields[i].name.toUpperCase() + "</TH>");
+				  html_body = html_body + "</TR>";
+
+                  //*** prints rows of table data
+				  for (var i = 0; i < result.length; i++)
+				       html_body = html_body + ("<TR><TD>" + result[i].title + "</TD>" + "<TD>" + result[i].description + "</TD></TR>");
+
+                  html_body = html_body + "</TABLE>";
+
+				  //** finish off the html body with a link back to the search page
+				  html_body = html_body + "<BR><BR><BR><a href=http://localhost:3000/search>Go Back To Search</a><BR><BR><BR>";
+			      html_body = html_body + "</BODY></HTML>";
+
+                console.log(html_body);             // send query results to the console
+			    res.send(html_body);                // send query results back to the browser
+	         }
+    });
+});
