57ff9939571170a77992a365c3857c86d32d00f3
Ryanfar0228 <148507483+Ryanfar0228@users.noreply.github.com>
2024-12-02 20:49:58 -0500

    Update dbConnection.js

--- LegoJS/dbConnection.js
+++ LegoJS/dbConnection.js
@@ -1,9 +1,11 @@
+require('dotenv').config(); // Load environment variables from .env file
 const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Replace w user
-  password: '', // Replace w pw
-  database: 'lego_db', // db name replace if needed
+  host: process.env.DB_HOST, // Loaded from .env
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
